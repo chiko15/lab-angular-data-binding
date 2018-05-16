b862c5a50872dfd93a79b4df5576cac0fb680a2d
Luis Flores <leinad15.df@gmail.com>
2018-05-16 11:14:55 -0400

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,33 +7,54 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foods: Object [];
-  newFood: any = {};
-  todaysFoods: Array<any> = [];
-  constructor() { }
+  searchTerm: String = "";
+  resultsArray: Array<any> = [];
+  foodList: Array<any> = [];
+  newFood:any = {name: "", calories: "", image: ""};
+  isFormShowing:Boolean = false;
+  todaysFoods:Array<any> = [];
+  todaysCalories: number = 0;
+
+  toggleForm(){
+    // if(this.isFormShowing === false){
+    //   this.isFormShowing = true
+    // } else {
+    //   this.isFormShowing = false
+    // }
+    this.isFormShowing = !this.isFormShowing;
+  }
+
+
+  addNewFood(){
+    const theNewFood = {
+      name: this.newFood.name,
+      calories: this.newFood.calories,
+      image: this.newFood.image
+    }
+    this.foodList.unshift(theNewFood);
+    this.newFood = {name: "", calories: "", image: ""};
+    this.isFormShowing = false;
+  }
+
+
+  addToList(theFood){
+    this.todaysFoods.unshift(theFood)
+    this.todaysCalories+= Number(theFood.calories);
+
+  }
+
+  filterFoods(){
+    this.resultsArray = this.foodList.filter((food=>{
+      return food.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    }))
+  }
+
+
+
 
   ngOnInit() {
-    this.foods = foods;
+    this.foodList = foods;
+    this.resultsArray = this.foodList;
   }
-
-  addFood(){
-    console.log("Add food has been called");
-    
-    const addFood = {name: this.newFood.name, calories: this.newFood.calories, image: this.newFood.image}
-      this.foods.push(addFood);
-    // clear inputs
-    this.newFood.name = "";
-    this.newFood.calories = "";
-    this.newFood.image = "";
-  }
-
-
-  selectFood(foodItem) {
-    this.todaysFoods.push(foodItem);
-  }
-
-
-
-
 
 }
